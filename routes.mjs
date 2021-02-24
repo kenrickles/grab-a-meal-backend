346fb9000c08845bdaf8c85fe07a1e0ba2f27c31
aljt <alvinljt@gmail.com>
2021-02-24 18:28:09 +0800

    added login route

--- routes.mjs
+++ routes.mjs
@@ -1,11 +1,13 @@
 import db from './models/index.mjs';
 
 // import your controllers here
+import initUsersController from './controllers/users.mjs';
 
 export default function bindRoutes(app) {
-
   // initialize the controller functions here
   // pass in the db for all callbacks
+  const UsersController = initUsersController(db);
 
   // define your route matchers here using app
+  app.post('/login', UsersController.login);
 }

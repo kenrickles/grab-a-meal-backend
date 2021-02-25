641e3bf3c89d1c76292f062ff12d398f13c51164
Kenrickles <kenrickles@gmail.com>
2021-02-25 12:52:37 +0800

    Added CheckAuth to routes and tested, it is working

--- routes.mjs
+++ routes.mjs
@@ -1,4 +1,5 @@
 import db from './models/index.mjs';
+import checkAuthMiddleware from './utilities/check-auth.mjs';
 
 // import your controllers here
 import initUsersController from './controllers/users.mjs';
@@ -7,12 +8,13 @@ import initActivityController from './controllers/activities.mjs';
 export default function bindRoutes(app) {
   // initialize the controller functions here
   // pass in the db for all callbacks
+  const checkAuth = checkAuthMiddleware(db);
   const UsersController = initUsersController(db);
   const ActivitiesController = initActivityController(db);
 
   // define your route matchers here using app
   app.post('/login', UsersController.login);
   app.post('/register', UsersController.register);
-  app.get('/activities', ActivitiesController.index);
+  app.get('/activities', checkAuth, ActivitiesController.index);
   app.post('/createActivities', ActivitiesController.createActivity);
 }
